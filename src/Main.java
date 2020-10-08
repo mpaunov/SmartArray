a16b799a1ece859385ba4efbb64aaa25769ecb5c
MartinPaunov <paunovmar@gmail.com>
2020-10-08 20:57:44 +0300

    Created LinkedList implementation.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
-        SmartArray smartArray = new SmartArray();
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
 
-        Stack stack = new Stack();
+        LinkedList linkedList = new LinkedList();
 
-        for (int i = 0; i < 10; i++) {
-            stack.push(i + 1);
+        int[] firstArr = linkedList.toArray();
+
+        for (int i = 0; i < 4; i++) {
+            linkedList.addLast(i + 1);
         }
 
-        stack.forEach(System.out::println);
+
+        System.out.println(linkedList.removeLast());
+        System.out.println(linkedList.removeLast());
+        System.out.println(linkedList.removeLast());
+        System.out.println(linkedList.removeLast());
+
+        linkedList.forEach(System.out::println);
     }
 }
